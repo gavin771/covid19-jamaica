25b77cdfc67d8785b7ac530a753625bdf8e62664
Gavin Samuels <gavin.samuels10@gmail.com>
2020-03-14 18:33:09 -0500

    moved loadcovid data to ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,18 +8,35 @@ const React = require("react");
 const safePrefix = require("./src/utils/safePrefix").default;
 
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+  setHeadComponents([
+    <script>
+      {async function loadCOVID() {
+        document.getElementById("last-update").innerHTML = "";
+        document.getElementById("confirmed").innerHTML = "";
+        document.getElementById("deaths").innerHTML = "";
+        document.getElementById("recovered").innerHTML = "";
+        const res = await fetch(
+          "https://covid19-jamaica.netlify.com/.netlify/functions/covid-api"
+        );
+        const data = await res.json();
+        document.getElementById("last-update").innerHTML = new Date(
+          data?.covid19Stats[0]?.lastUpdate
+        ).toDateString();
+        document.getElementById("confirmed").innerHTML =
+          data?.covid19Stats[0]?.confirmed;
+        document.getElementById("deaths").innerHTML =
+          data?.covid19Stats[0]?.deaths;
+        document.getElementById("recovered").innerHTML =
+          data?.covid19Stats[0]?.recovered;
+      }}
+    </script>
+  ]);
 
-    setHeadComponents([
-        
-    ]);
-
-    setPostBodyComponents([
-        <React.Fragment>
-            <script src={safePrefix('assets/js/plugins.js')}/>
-            <script src={safePrefix('assets/js/prism.js')} data-manual/>
-            <script src={safePrefix('assets/js/main.js')}/>
-            
-        </React.Fragment>
-    ]);
-
+  setPostBodyComponents([
+    <React.Fragment>
+      <script src={safePrefix("assets/js/plugins.js")} />
+      <script src={safePrefix("assets/js/prism.js")} data-manual />
+      <script src={safePrefix("assets/js/main.js")} />
+    </React.Fragment>
+  ]);
 };
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -132,25 +132,6 @@ window.onGatsbyRouteUpdate = function() {
     return elements;
   }
 
-  async function loadCOVID() {
-    document.getElementById("last-update").innerHTML = "";
-    document.getElementById("confirmed").innerHTML = "";
-    document.getElementById("deaths").innerHTML = "";
-    document.getElementById("recovered").innerHTML = "";
-    const res = await fetch(
-      "https://covid19-jamaica.netlify.com/.netlify/functions/covid-api"
-    );
-    const data = await res.json();
-    document.getElementById("last-update").innerHTML = new Date(
-      data?.covid19Stats[0]?.lastUpdate
-    ).toDateString();
-    document.getElementById("confirmed").innerHTML =
-      data?.covid19Stats[0]?.confirmed;
-    document.getElementById("deaths").innerHTML = data?.covid19Stats[0]?.deaths;
-    document.getElementById("recovered").innerHTML =
-      data?.covid19Stats[0]?.recovered;
-  }
-
   function createLinksForHeaderElements(elements) {
     let result = [];
     let stack = [
