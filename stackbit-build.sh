6f5949aeaeafc64bd2cfb7b511780b767ebc7d70
Gavin Samuels <gavin.samuels10@gmail.com>
2020-03-14 15:52:52 -0500

    updated build steps

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -7,5 +7,5 @@ set -v
 curl -s -X POST https://api.stackbit.com/project/5e6b8ee447bcae0013d5674f/webhook/build/pull > /dev/null
 npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e6b8ee447bcae0013d5674f
 curl -s -X POST https://api.stackbit.com/project/5e6b8ee447bcae0013d5674f/webhook/build/ssgbuild > /dev/null
-gatsby build
+npm run build
 curl -s -X POST https://api.stackbit.com/project/5e6b8ee447bcae0013d5674f/webhook/build/publish > /dev/null
